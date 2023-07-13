eb0903dd58d1e66a8018486b7a6a8d652541c377
Denny Kluge <denny.kluge@gmail.com>
2023-07-13 23:00:27 +0200

    finished "architecting your compose ui"

--- app/src/main/java/com/example/unscramble/ui/GameViewModel.kt
+++ app/src/main/java/com/example/unscramble/ui/GameViewModel.kt
@@ -1,47 +1,57 @@
 package com.example.unscramble.ui
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import com.example.unscramble.data.allWords
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 
-
-private val _uiState = MutableStateFlow(GameUiState())
-val uiState: StateFlow<GameUiState> = _uiState.asStateFlow()
-
-private lateinit var currentWord: String
-private var usedWords: MutableSet<String> = mutableSetOf()
-
-private fun pickRandomWordAndShuffle(): String {
-    currentWord = allWords.random()
-    return if (usedWords.contains(currentWord)) {
-        pickRandomWordAndShuffle()
-    } else {
-        usedWords.add(currentWord)
-        shuffleCurrentWord(currentWord)
-    }
-}
-
-fun shuffleCurrentWord(word: String): String {
-    val charArray = word.toCharArray()
-    charArray.shuffle()
-    while (String(charArray).equals(word)) {
-        charArray.shuffle()
-    }
-
-    return String(charArray)
-}
-
-
-fun resetGame() {
-    usedWords.clear()
-    _uiState.value = GameUiState(pickRandomWordAndShuffle())
-}
-
 class GameViewModel : ViewModel() {
+
+    private val _uiState = MutableStateFlow(GameUiState())
+    val uiState: StateFlow<GameUiState> = _uiState.asStateFlow()
+
+    private lateinit var currentWord: String
+    private var usedWords: MutableSet<String> = mutableSetOf()
+
+    var userGuess by mutableStateOf("")
+        private set
 
     init {
         resetGame()
+    }
+
+
+    private fun pickRandomWordAndShuffle(): String {
+        currentWord = allWords.random()
+        return if (usedWords.contains(currentWord)) {
+            pickRandomWordAndShuffle()
+        } else {
+            usedWords.add(currentWord)
+            shuffleCurrentWord(currentWord)
+        }
+    }
+
+    fun shuffleCurrentWord(word: String): String {
+        val charArray = word.toCharArray()
+        charArray.shuffle()
+        while (String(charArray).equals(word)) {
+            charArray.shuffle()
+        }
+
+        return String(charArray)
+    }
+
+
+    private fun resetGame() {
+        usedWords.clear()
+        _uiState.value = GameUiState(pickRandomWordAndShuffle())
+    }
+
+    fun updateUserGuess(guessedWord: String) {
+        userGuess = guessedWord
     }
 }
